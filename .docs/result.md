479519ab53d2f71e77275d520a37709b7026c69f
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-05-24 00:16:13 +0900

    【Red】対応する公開作品が存在しない年号の場合

---
+++ .docs/result.md
@@ -0,0 +1,10 @@
+- 【Red】対応する公開作品が存在しない場合
+
+```
+There was 1 error:
+
+1) Tests\Unit\Sample\SampleTest::getFilmName関数は、対応する公開作品が無い年号の末尾の数字を与えると引退宣言を返す
+Error: Call to undefined method App\Services\Sample\SampleService::getFilmName()
+
+/app/tests/Unit/Sample/SampleTest.php:28
+```
--- php/tests/Unit/Sample/SampleTest.php
+++ php/tests/Unit/Sample/SampleTest.php
@@ -19,4 +19,15 @@ class SampleTest extends TestCase
         $this->app->instance(SampleServiceInterface::class, $this->sampleService);
     }
 
+    /**
+     * @test
+     */
+    public function getFilmName関数は、対応する公開作品が無い年号の末尾の数字を与えると引退宣言を返す()
+    {
+        $expected = '次の作品が完成したら今度こそ引退します';
+        $actual   = $this->sampleService->getFilmName(1);
+
+        $this->assertSame($expected, $actual);
+    }
+
 }
