62e71f7e0c7666efcedec1aadec1980b5df20c79
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-05-24 23:35:01 +0900

    【Refactor】プロダクトコード：switchを使う？

--- php/app/Services/Sample/SampleService.php
+++ php/app/Services/Sample/SampleService.php
@@ -29,14 +29,21 @@ class SampleService implements SampleServiceInterface
      */
     public function getFilmNameFromYear(int $year): string
     {
-        if ($year === 1986) {
-            return '天空の城ラピュタ';
-        } else if ($year === 1988) {
-            return 'となりのトトロ';
-        } else if ($year === 1989) {
-            return '魔女の宅急便';
+        switch($year) {
+            case 1986:
+                $result = '天空の城ラピュタ';
+                break;
+            case 1988:
+                $result = 'となりのトトロ';
+                break;
+            case 1989:
+                $result = '魔女の宅急便';
+                break;
+            default:
+                $result = '次の作品が完成したら今度こそ引退します';
+                break;
         }
-        return '次の作品が完成したら今度こそ引退します';
+        return $result;
     }
 
 }
